8493441f3a9bc5c552b5cea5e602a5286e3c15fb
hreeves <hreeves@152.23.54.92>
2019-08-29 10:06:29 -0400

    novice project

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,41 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Integer count of number of customers
+		
+		int count = scan.nextInt();
+		
+		
+		// array of customer data
+		double total = 0.0;
+				
+		for (int i=0; i<count; i++) {
+			String first = scan.next();
+			String last = scan.next();
+			int number_of_items = scan.nextInt();
+			for (int q=0; q<number_of_items; q++) {
+				int quantity_of_item = scan.nextInt();
+				String name_of_item = scan.next();
+				double price_of_item = scan.nextDouble();
+				double item_total_price = quantity_of_item * price_of_item;
+				total = total + item_total_price;
+			}
+			
+			System.out.println(first.substring(0,1) + ". " + last + " " + String.format("%.2f", ((double) total) ));
+			
+			total = 0;
+			
+
+		
+		} 
+		
+		
+		scan.close();
+		
+		
 	}
+	
+	
 }
+
+
